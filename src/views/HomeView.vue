82b51a96c7653449ed0e45a38c809445f81c3a51
Luis Arellano <72470438+learellanom@users.noreply.github.com>
2022-04-28 23:21:50 -0400

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="Cidenet logo" src="../assets/cidenetlogos.png">
+    <br>
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+</script>
+<style>
+.home {
+    text-align: center;
+  }
+</style>
